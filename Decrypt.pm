8e31dba62ba4ce96a2623b5d4ffddcccf9b2b48b
Sam Nicholls <sam.n@studio8media.co.uk>
2012-09-20 02:41:39 +0100

    First Commit

---
+++ Decrypt.pm
@@ -0,0 +1,91 @@
+#Copyright (c) 2012 Genome Research Ltd. 
+#Author: Sam Nicholls <sn8@sanger.ac.uk>
+#
+#This file is part of IYG Web.
+#
+#IYG Web is free software: you can redistribute it and/or modify it under 
+#the terms of the GNU Affero General Public License as published by the Free 
+#Software Foundation; either version 3 of the License, or (at your option) any 
+#later version. 
+#
+#This program is distributed in the hope that it will be useful, but WITHOUT 
+#ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+#FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more 
+#details.
+#
+#You should have received a copy of the GNU Affero General Public License along
+#with this program. If not, see <http://www.gnu.org/licenses/>.
+
+package IYG::Decrypt;
+
+use Moose;
+use Crypt::OpenPGP;
+
+has secret => (
+    is => 'ro',
+    isa => 'Str',
+    required => 1
+);
+
+has message => (
+    is => 'ro',
+    isa => 'Str',
+    required => 1
+);
+
+sub decrypt{
+    my $self = shift;
+
+    # Decrypt
+    my $pgp = Crypt::OpenPGP->new;
+    my($pt, $valid, $sig) = $pgp->decrypt(
+        Data => $self->message,
+        Passphrase => $self->secret
+    );
+    die ("Sorry. Barcode decryption failed... ", $pgp->errstr) unless $pt;
+    return $pt;
+}
+
+no Moose;
+1;
+__END__
+
+=head1 NAME
+
+Decrypt - PGP Message Decrypter
+
+=head1 DESCRIPTION
+
+Utilizes the Crypt::OpenPGP library to decrypt PGP encoded messages sent by
+the barcode login script.
+
+=head1 ATTRIBUTES
+
+=over 4
+
+=item *
+
+=head3 secret
+
+The passphrase for the private key that forms a pair with the public key
+used to encrypt the message.
+
+=head3 message
+
+The PGP encoded message to be decrypted.
+
+=back
+
+=head1 METHODS
+
+=over 4
+
+=item *
+
+=head3 decrypt
+
+Decrypt and return the result (in this context; a barcode)
+
+=back
+
+=cut
