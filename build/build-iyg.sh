29ac4f5236596a66aa71b4c962f16e8ddaca861f
Joshua C. Randall <jcrandall@alum.mit.edu>
2012-12-02 22:36:55 +0000

    added build script to drop and create database and moved iyg.sql from web out to top-level

---
+++ build/build-iyg.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+usage="
+Usage: ./build-iyg.sh ~/iyg-private-data/
+"
+
+IYG_PRIVATE_DATA_DIR="$1"
+IYG_CREATE_DB_SQL="../iyg.sql"
+IYG_DB_NAME="iyg"
+
+if [[ -n "${IYG_PRIVATE_DATA_DIR}" ]] && \
+    [[ -d ${IYG_PRIVATE_DATA_DIR} ]] 
+then
+    if [[ -f ${IYG_PRIVATE_DATA_DIR}/mysql-user-pass.txt && \
+	  -f ${IYG_PRIVATE_DATA_DIR}/mysql-iygadmin.cnf ]] 
+    then
+        # drop database and re-create
+	echo "DROP SCHEMA ${IYG_DB_NAME};" | mysql --defaults-extra-file=${IYG_PRIVATE_DATA_DIR}/mysql-iygadmin.cnf
+	cat ${IYG_CREATE_DB_SQL} | mysql --defaults-extra-file=${IYG_PRIVATE_DATA_DIR}/mysql-iygadmin.cnf 
+    else 
+	echo "Required files do not exist in ${IYG_PRIVATE_DATA_DIR}"
+	exit 2
+    fi
+else 
+    echo "${usage}"
+    exit 1
+fi
+
+
