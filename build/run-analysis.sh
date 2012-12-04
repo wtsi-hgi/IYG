92c03e542a4d122a1703af05a4d8d8bdd8bec00f
Jeffrey C. Barrett <jcbarret@gmail.com>
2012-12-04 17:01:09 +0000

    PCA now pipelined

--- build/run-analysis.sh
+++ build/run-analysis.sh
@@ -10,6 +10,7 @@ else
     IYG_DIR=`pwd`/${reldir}/../
 fi
 PUB_DATA_DIR=${IYG_DIR}/public_data/
+PRED_DATA_DIR=${PUB_DATA_DIR}/pred_results/
 
 if [[ ! -e ${PRIV_DATA_DIR}/iyg.ped ]]
     then
@@ -17,12 +18,14 @@ if [[ ! -e ${PRIV_DATA_DIR}/iyg.ped ]]
     exit 1
 fi
 
+##########################
 #1. run QC on raw TSV
 #calculate missing rate by plate
 #analyse/qc/missing-by-plate.pl
 #produce missing-by-plate.txt
 #MANUAL: look at output and produce a list of SNPs called failed-snps.txt
 
+##########################
 #2. run QC on Josh's input PED file
 #remove failed SNPs
 ###p-link --noweb --file ALL_assay_summary.publicid.masterplink --missing-genotype N --exclude failed-snps.txt --make-bed --out iyg-1
@@ -42,59 +45,74 @@ fi
 #create input pedfile for DB load
 ###p-link --noweb --bfile iyg-2 --recode --out iyg
 
+mkdir -p ${PRED_DATA_DIR}
+
+##########################
 #3. generate ABO predictions
-#p-link --noweb --file ALL_assay_summary.publicid.masterplink --missing-genotype N --snps rs8176743,rs8176746,rs8176747,rs8176719 --recode --out abo
-(cd ${PRIV_DATA_DIR} && p-link --noweb --file iyg --missing-genotype 0 --snps rs8176743,rs8176746,rs8176747,rs8176719 --recode --out abo)
-${IYG_DIR}/analyse/abo/abo-matic.pl ${PRIV_DATA_DIR}/abo.ped > ${PRIV_DATA_DIR}/abo-matic.txt
-
 #MANUAL/OPTIONAL: analyse/abo/abo-avg.pl can be used in looking at dirty intensities
+#put in public_data/pred_results/
+mkdir ${PRED_DATA_DIR}/ABO/
+(cd ${PRIV_DATA_DIR} && p-link --noweb --file iyg --missing-genotype 0 --snps rs8176743,rs8176746,rs8176747,rs8176719 --recode --out abo)
+${IYG_DIR}/analyse/abo/abo-matic.pl ${PRIV_DATA_DIR}/abo.ped > ${PRED_DATA_DIR}/ABO/abo-matic.txt
 
+##########################
 #4. generate sex predictions
 #use analyse/sex/sex-intensity.pl to produce sex-info.txt file
 #MANUAL: use analyse/sex/sexer.R to produce sex assignments as sexpred.txt
 
+##########################
 #5. generate Y predictions
 #NOTE! This currently is not easily pipelineable. We can add the processed files for v1, and discuss options for v2. We'll need to do a pi->barcode transform, though.
 
+##########################
 #6. generate PCA predictions
-#MANUAL: create worldpca.txt
-R --no-restore --no-save --args ${PRIV_DATA_DIR}/worldpca.txt ${PRIV_DATA_DIR}/pca/ < ${IYG_DIR}/analyse/pca/plotPCA.R 
+mkdir ${PRED_DATA_DIR}/AIM/
+#create merged file for PCA
+p-link --noweb --bfile ${PUB_DATA_DIR}/pca/1KGdata --merge ${PRIV_DATA_DIR}/iyg.ped ${PRIV_DATA_DIR}/iyg.map --extract ${PUB_DATA_DIR}/pca/PCAsnps.txt --out ${PRED_DATA_DIR}/AIM/1KG_IYG_merged --make-bed
 
+#run PCA
+R --no-restore --no-save --args ${PRED_DATA_DIR}/AIM ${PUB_DATA_DIR}/pca <${IYG_DIR}/analyse/pca/doPCA.R
+
+#make plots
+R --no-restore --no-save --args ${PRED_DATA_DIR}/AIM/worldpca.txt ${PRED_DATA_DIR}/AIM/ < ${IYG_DIR}/analyse/pca/plotPCA.R 
+
+##########################
 #7. generate MT predictions
 #NOTE! This currently is not easily pipelineable. We can add the processed files for v1, and discuss options for v2. We'll need to do a pi->barcode transform, though.
 
+##########################
 #8. generate QT predictions
 #in directory with mangroveinput.ped, mangroveinput.map, *.grovebeta
 #Standard QTs
-##mkdir BMI 
-##R --no-restore --no-save --args BMI <mangrove-it.R
-##mkdir BP
-##R --no-restore --no-save --args BP <mangrove-it.R
-##mkdir FPG 
-##R --no-restore --no-save --args FPG <mangrove-it.R
-##mkdir HDLC 
-##R --no-restore --no-save --args HDLC <mangrove-it.R
-##mkdir MPV 
-##R --no-restore --no-save --args MPV <mangrove-it.R
-##mkdir SMOK 
-##R --no-restore --no-save --args SMOK <mangrove-it.R
-##mkdir TC 
-##R --no-restore --no-save --args TC <mangrove-it.R
-##mkdir WHR 
-##R --no-restore --no-save --args WHR <mangrove-it.R
+mkdir ${PRED_DATA_DIR}/BMI/ 
+R --no-restore --no-save --args BMI <${IYG_DIR}/analyse/qt/mangrove-it.R
+mkdir ${PRED_DATA_DIR}/BP/
+R --no-restore --no-save --args BP <${IYG_DIR}/analyse/qt/mangrove-it.R
+mkdir ${PRED_DATA_DIR}/FPG/
+R --no-restore --no-save --args FPG <${IYG_DIR}/analyse/qt/mangrove-it.R
+mkdir ${PRED_DATA_DIR}/HDLC/
+R --no-restore --no-save --args HDLC <${IYG_DIR}/analyse/qt/mangrove-it.R
+mkdir ${PRED_DATA_DIR}/MPV/
+R --no-restore --no-save --args MPV <${IYG_DIR}/analyse/qt/mangrove-it.R
+mkdir ${PRED_DATA_DIR}/SMOK/ 
+R --no-restore --no-save --args SMOK <${IYG_DIR}/analyse/qt/mangrove-it.R
+mkdir ${PRED_DATA_DIR}/TC/
+R --no-restore --no-save --args TC <${IYG_DIR}/analyse/qt/mangrove-it.R
+mkdir ${PRED_DATA_DIR}/WHR/
+R --no-restore --no-save --args WHR <${IYG_DIR}/analyse/qt/mangrove-it.R
 
 #this kind of sucks
-##mkdir BALD 
-##R --no-restore --no-save --args BALD <mangrove-it.R
+mkdir ${PRED_DATA_DIR}/BALD 
+R --no-restore --no-save --args BALD <${IYG_DIR}/analyse/qt/mangrove-it.R
 
-###mkdir NEAND 
-#still broken
-#R --no-restore --no-save --args NEAND <mangrove-it.R
+mkdir ${PRED_DATA_DIR}/NEAND 
+##still broken
+R --no-restore --no-save --args NEAND <${IYG_DIR}/analyse/qt/mangrove-it.R
 
-#Special QTs
-##mkdir CAFE 
-##R --no-restore --no-save <mangrove-it-CAFE.R
-##mkdir EYE 
-##R --no-restore --no-save <mangrove-it-EYE.R
+#Special QTs, this is sloppy.
+mkdir ${PRED_DATA_DIR}/CAFE 
+R --no-restore --no-save <${IYG_DIR}/analyse/qt/mangrove-it-CAFE.R
+mkdir ${PRED_DATA_DIR}/EYE 
+R --no-restore --no-save <${IYG_DIR}/analyse/qt/mangrove-it-EYE.R
 
 
