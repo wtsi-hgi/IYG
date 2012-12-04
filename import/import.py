bd1559f256e3de29b075670f55dd12553859cf7c
Martin Pollard <mp15@sanger.ac.uk>
2012-12-04 16:27:46 +0000

    fix extra newline in output

--- import/import.py
+++ import/import.py
@@ -646,7 +646,7 @@ class Data_Loader:
             self.cur.execute("select barcode, public_id from profiles")
             res = self.cur.fetchall()
             for profile in res:
-                print>>profile_output, "%s\t%s\n" % (profile[0], profile[1])
+                print>>profile_output, "%s\t%s" % (profile[0], profile[1])
         except MySQLdb.Error, e:
             print "[WARN]\tSNP genotype frequency query failed for snp %s" % snp_dbid
             print "\tError %d: %s" % (e.args[0], e.args[1])
