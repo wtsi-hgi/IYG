97081228a49e7e12684d52b5ae97712c21990c2b
Joshua Randall <jcrandall@alum.mit.edu>
2012-12-04 02:47:59 +0000

    Added handling of absolute and relative paths

--- build/build-all.sh
+++ build/build-all.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 PRIV_DATA_DIR=$1
-IYG_DIR=`pwd`/`dirname $0`/../
+reldir=`dirname $0`
+if [[ `echo "${reldir}" | cut -c1` = "/" ]]
+then
+    IYG_DIR=${reldir}/../
+else
+    IYG_DIR=`pwd`/${reldir}/../
+fi
 PUB_DATA_DIR=${IYG_DIR}/public_data/
 
 if [[ ! -e ${PRIV_DATA_DIR}/iyg.ped ]]
