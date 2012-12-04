9c1ce40905d766bfb1c3d039198ad20831654671
Jeffrey C. Barrett <jcbarret@gmail.com>
2012-12-04 16:19:42 +0000

    fiddling with pipelines.

--- build/run-analysis.sh
+++ build/run-analysis.sh
@@ -25,28 +25,29 @@ fi
 
 #2. run QC on Josh's input PED file
 #remove failed SNPs
-###p-link --noweb --file ALL_assay_summary.fakebarcode.masterplink --missing-genotype N --exclude failed-snps.txt --make-bed --out iyg-1
+###p-link --noweb --file ALL_assay_summary.publicid.masterplink --missing-genotype N --exclude failed-snps.txt --make-bed --out iyg-1
 
 #view missing rates in this file (autosomes only)
 #MANUAL: produce sample-fails.txt for exclusions (currently MISS > 0.5)
 #MANUAL: produce flagged-samples.txt for flagging (currently 0.05 > MISS > 0.50)
 
 #MANUAL: negative-snps.txt is file of SNPs on - strand in Source file
-#negative-snps.txt can be reproduced using matchalleles.pl, ensembl-alleles.txt, compare-alleles.pl,
-#snpseqs.txt
+#analyse/qc/negative-snps.txt can be reproduced using matchalleles.pl, ensembl-alleles.txt, compare-alleles.pl, snpseqs.txt
 
 #removed failed samples, flip strands
 ###p-link --noweb --bfile iyg-1 --remove sample-fails.txt --flip negative-snps.txt --make-bed --out iyg-2
 
 #MANUAL: max-ibs.pl can be used, along with PLINK --genome, to find duplicates.
 
+#create input pedfile for DB load
+###p-link --noweb --bfile iyg-2 --recode --out iyg
+
 #3. generate ABO predictions
-#p-link --noweb --file ALL_assay_summary.fakebarcode.masterplink --missing-genotype N --snps rs8176743,rs8176746,rs8176747,rs8176719 --recode --out abo
+#p-link --noweb --file ALL_assay_summary.publicid.masterplink --missing-genotype N --snps rs8176743,rs8176746,rs8176747,rs8176719 --recode --out abo
 (cd ${PRIV_DATA_DIR} && p-link --noweb --file iyg --missing-genotype 0 --snps rs8176743,rs8176746,rs8176747,rs8176719 --recode --out abo)
 ${IYG_DIR}/analyse/abo/abo-matic.pl ${PRIV_DATA_DIR}/abo.ped > ${PRIV_DATA_DIR}/abo-matic.txt
 
 #MANUAL/OPTIONAL: analyse/abo/abo-avg.pl can be used in looking at dirty intensities
-#generate abo predictions using abo-matic.pl
 
 #4. generate sex predictions
 #use analyse/sex/sex-intensity.pl to produce sex-info.txt file
