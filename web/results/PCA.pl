wtsi-hgi/IYG

commit 7626a1e8d590e4f9106bd98d869034948f28e4a0

Fix finish on undefined error

web/results/PCA.pl
@@ -120,7 +120,7 @@ else{
     }
 
     $snpResultSet->finish;
-    $variantGenotypeResultSet->finish;
+    if (defined($variantGenotypeResultSet) { $variantGenotypeResultSet->finish; }
     $traitResultSet->finish;
     $app->dbh->disconnectDb();
 }

web/results/ABO.pl
@@ -123,7 +123,7 @@ else{
     }
 
     $snpResultSet->finish;
-    $variantGenotypeResultSet->finish;
+    if (defined($variantGenotypeResultSet)) { $variantGenotypeResultSet->finish; }
     $traitResultSet->finish;
     $app->dbh->disconnectDb();
 }

web/results/QT1.pl
@@ -128,7 +128,7 @@ else{
     }
 
     $snpResultSet->finish;
-    $variantGenotypeResultSet->finish;
+    if (defined($variantGenotypeResultSet)) { $variantGenotypeResultSet->finish; }
     $traitResultSet->finish;
     $app->dbh->disconnectDb();
 }

web/results/QT2.pl
@@ -143,7 +143,7 @@ else{
     }
 
     $snpResultSet->finish;
-    $variantGenotypeResultSet->finish;
+    if (defined($variantGenotypeResultSet)) { $variantGenotypeResultSet->finish; }
     $traitResultSet->finish;
     $app->dbh->disconnectDb();
 }

web/results/SingleSNP.pl
@@ -108,7 +108,7 @@ else{
     }
 
     $snpResultSet->finish;
-    $variantGenotypeResultSet->finish;
+    if (defined($variantGenotypeResultSet)) { $variantGenotypeResultSet->finish; }
     $traitResultSet->finish;
     $app->dbh->disconnectDb();
 }

web/results/Tree.pl
@@ -128,7 +128,7 @@ else{
     }
 
     $snpResultSet->finish;
-    $variantGenotypeResultSet->finish;
+    if (defined($variantGenotypeResultSet)) { $variantGenotypeResultSet->finish; }
     $traitResultSet->finish;
     $app->dbh->disconnectDb();
 }

web/results/index.pl
@@ -108,7 +108,7 @@ else{
     }
 
     $snpResultSet->finish;
-    $variantGenotypeResultSet->finish;
+    if (defined($variantGenotypeResultSet) { $variantGenotypeResultSet->finish; }
     $traitResultSet->finish;
     $app->dbh->disconnectDb();
 }

commit 6b1c0c5a80ebc35cb6446f72d6df63f1d44564dc

Fixed syntax error introduced by 7626a1e8 -- missing right paren

web/results/PCA.pl
@@ -120,7 +120,9 @@ else{
     }
 
     $snpResultSet->finish;
-    if (defined($variantGenotypeResultSet) { $variantGenotypeResultSet->finish; }
+    if (defined($variantGenotypeResultSet)) { 
+	$variantGenotypeResultSet->finish; 
+    }
     $traitResultSet->finish;
     $app->dbh->disconnectDb();
 }